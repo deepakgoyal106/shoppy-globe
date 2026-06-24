30fbbf4176b561559bc93b6d7cdf6ecb30280a21
deepakgoyal106 <deepakgoyal106@gmail.com>
2026-06-24 16:23:50 +0530

    checkout

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,23 +1,80 @@
-import { useLocation, Link } from "react-router-dom";
+// NotFound page
+// Displays error when user visits an invalid route
+// Provides link back to Home page
+
+
+import { Link, useLocation } from "react-router-dom";
+
+
 
 function NotFound() {
 
+
+
+  // Get current invalid URL
+
   const location = useLocation();
 
+
+
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
+
+    <div className="not-found">
+
+
+
+      {/* Error heading */}
+
+      <h1>
+
+        404 - Page Not Found
+
+      </h1>
+
+
+
+
+      {/* Display wrong route */}
 
       <p>
-        Invalid Route:
-        {location.pathname}
+
+        The page
+
+        <b>
+
+          {location.pathname}
+
+        </b>
+
+        does not exist.
+
       </p>
 
+
+
+
+      {/* Go back home */}
+
       <Link to="/">
-        Go Home
+
+
+        <button>
+
+          Go Home
+
+        </button>
+
+
       </Link>
+
+
+
     </div>
+
   );
+
 }
+
+
 
 export default NotFound;
