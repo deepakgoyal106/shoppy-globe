2967dde1acf4fb5137a0c99d8189a2aff91f76ff
deepakgoyal106 <deepakgoyal106@gmail.com>
2026-06-24 16:20:27 +0530

    cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,81 +1,146 @@
+// Cart page
+// Displays all products added to cart
+// Allows user to go to checkout
+
+
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+
 import CartItem from "../components/CartItem";
 
 
+
 function Cart() {
+
+
+
+  // Get cart products from Redux store
 
   const cartItems = useSelector(
     (state) => state.cart.cartItems
   );
 
 
+
+  // Calculate total cart amount
+
   const totalPrice = cartItems.reduce(
-    (total,item) =>
+
+    (total, item) =>
+
       total + item.price * item.quantity,
+
     0
+
   );
+
 
 
   return (
 
     <div className="cart-container">
 
+
+      {/* Page title */}
+
       <h2>
         Shopping Cart
       </h2>
 
 
+
       {
-        cartItems.length === 0
 
-        ?
 
-        <h3>
-          Cart is empty
-        </h3>
+        // If cart is empty show message
+
+        cartItems.length === 0 ?
+
+
+        (
+
+          <h3>
+            Cart is empty
+          </h3>
+
+        )
+
 
         :
 
-        <>
+
+        (
+
+          <>
 
 
-        <div className="cart-list">
+            {/* Render all cart items */}
 
-          {
-            cartItems.map((item)=>(
-
-              <CartItem
-                key={item.id}
-                item={item}
-              />
-
-            ))
-          }
-
-        </div>
+            <div className="cart-list">
 
 
-        <div className="cart-total">
+              {
 
-          <h3>
-           Total: ${totalPrice.toFixed(2)}
-          </h3>
+                cartItems.map((item)=>(
 
 
-          <Link to="/checkout">
+                  <CartItem
 
-            <button>
-              Checkout
-            </button>
+                    key={item.id}
 
-          </Link>
+                    item={item}
 
-
-        </div>
+                  />
 
 
-        </>
+                ))
+
+              }
+
+
+            </div>
+
+
+
+
+            {/* Cart total section */}
+
+            <div className="cart-total">
+
+
+              <h3>
+
+                Total:
+                ${totalPrice.toFixed(2)}
+
+              </h3>
+
+
+
+
+              {/* Go to checkout page */}
+
+              <Link to="/checkout">
+
+
+                <button>
+
+                  Checkout
+
+                </button>
+
+
+              </Link>
+
+
+            </div>
+
+
+
+          </>
+
+        )
+
 
       }
 
@@ -83,6 +148,7 @@ function Cart() {
     </div>
 
   );
+
 }
 
 
