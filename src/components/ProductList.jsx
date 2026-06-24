ea696733d4ab677a51e90df867369cc8b307b985
deepakgoyal106 <deepakgoyal106@gmail.com>
2026-06-24 16:14:01 +0530

    produtList

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,39 +1,72 @@
+// ProductList component
+// Receives products from parent component
+// Filters products according to search text
+// Renders multiple ProductItem components
+
+
 import { useSelector } from "react-redux";
+
 import ProductItem from "./ProductItem";
 
 
 function ProductList({ products }) {
 
 
+
+  // Getting search text from Redux store
+
   const searchTerm = useSelector(
     (state) => state.search.searchTerm
   );
 
 
-  const filteredProducts = products.filter(
-    (product) =>
+
+  // Filter products based on user search
+
+  const filteredProducts =
+    products.filter((product) =>
+
       product.title
-        .toLowerCase()
-        .includes(searchTerm.toLowerCase())
-  );
+      .toLowerCase()
+      .includes(
+        searchTerm.toLowerCase()
+      )
+
+    );
+
 
 
   return (
+
     <div className="products">
+
+
+      {/* 
+        Map through products array
+        and create reusable ProductItem component
+      */}
 
       {
         filteredProducts.map((product) => (
 
+
           <ProductItem
+
             key={product.id}
+
             product={product}
+
           />
+
 
         ))
       }
 
+
     </div>
+
   );
+
 }
 
 
