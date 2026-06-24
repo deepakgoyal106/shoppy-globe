22e3a6a6507305b0094646978fd04ee31232f726
deepakgoyal106 <deepakgoyal106@gmail.com>
2026-06-24 16:10:55 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
+// App is the main component of the application
+// It contains the common layout shared by all pages
+
+import { Outlet } from "react-router-dom";
+
+// Header is displayed on every page
 import Header from "./components/Header";
-import {Outlet} from "react-router-dom";
+
 
 function App() {
+
+
   return (
+
     <>
+
+      {/* Navigation header */}
       <Header />
-      <Outlet/>
+
+
+      {/* 
+        Outlet renders the current route component
+        Example:
+        Home, Cart, Checkout, ProductDetail
+      */}
+      <Outlet />
+
+
     </>
+
   );
+
 }
+
 
 export default App;
