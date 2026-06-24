ee73cd43cde27a0ec609570ff1bc84ea3f6ea9a2
deepakgoyal106 <deepakgoyal106@gmail.com>
2026-06-24 16:27:58 +0530

    search slice

--- src/redux/slices/searchSlice.js
+++ src/redux/slices/searchSlice.js
@@ -1,16 +1,67 @@
-import { createSlice } from '@reduxjs/toolkit'
+// Search slice
+// Stores search text globally using Redux
+// ProductList reads this value to filter products
+
+
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const initialState = {
+
+
+  // Current search value
+
+  searchTerm:""
+
+
+};
+
+
+
 
 const searchSlice = createSlice({
-  name: 'search',
-  initialState: {
-    searchTerm: "",
-  },
-  reducers: {
-   setSearchTerm:(state, action) => {
-    state.searchTerm = action.payload;
-   },
-  },
+
+
+  name:"search",
+
+
+  initialState,
+
+
+
+  reducers:{
+
+
+
+    // Update search text whenever user types
+
+    setSearchTerm:(state,action)=>{
+
+
+      state.searchTerm =
+      action.payload;
+
+
+    }
+
+
+
+  }
+
+
 });
 
-export const { setSearchTerm } = searchSlice.actions
+
+
+
+export const {
+
+  setSearchTerm
+
+
+} = searchSlice.actions;
+
+
+
 export default searchSlice.reducer;
