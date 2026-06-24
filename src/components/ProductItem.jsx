4d2c45b3301a379b396156161c21cc66deac8d75
deepakgoyal106 <deepakgoyal106@gmail.com>
2026-06-24 16:18:29 +0530

    productItem

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,20 +1,34 @@
+// ProductItem component
+// Displays a single product card
+// Handles add to cart action
+// Navigates to product details page
+
+
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 import { addToCart } from "../redux/slices/cartSlice";
 
 
 function ProductItem({ product }) {
 
+
+  // Redux dispatch function
   const dispatch = useDispatch();
+
+
+  // Controls add to cart popup visibility
   const [showMsg, setShowMsg] = useState(false);
+
 
 
   return (
 
     <div className="product-card">
 
+
+      {/* Product image */}
 
       <img
         loading="lazy"
@@ -23,52 +37,97 @@ function ProductItem({ product }) {
       />
 
 
+
+      {/* Product name */}
+
       <h3>
         {product.title}
       </h3>
 
 
-      <p>
-         ${product.price.toFixed(2)}
-      </p>
-      
 
+      {/* Product price */}
+
+      <p>
+        ${product.price.toFixed(2)}
+      </p>
+
+
+
+      {/* Product rating */}
+
+      <div className="rating">
+
+        {"⭐".repeat(
+          Math.round(product.rating)
+        )}
+
+        <span>
+          ({product.rating})
+        </span>
+
+      </div>
+
+
+
+      {/* Add product to Redux cart */}
 
       <button
-        onClick={() => {
 
-        dispatch(addToCart(product));
+        onClick={()=>{
 
-        setShowMsg(true);
+          dispatch(
+            addToCart(product)
+          );
 
-        setTimeout(() => {
+
+          setShowMsg(true);
+
+
+          setTimeout(()=>{
+
             setShowMsg(false);
-        }, 1500);
+
+          },1500);
 
         }}
-        >
+
+      >
+
         Add To Cart
-        </button>
-        {
-            showMsg &&
-            <p className="cart-popup">
-                Added To Cart ✅
-            </p>
-        }
 
-      <br />
+      </button>
 
+
+
+      {/* Show success popup */}
+
+      {
+        showMsg &&
+
+        <p className="cart-popup">
+          Added To Cart ✅
+        </p>
+
+      }
+
+
+
+      {/* Dynamic product details route */}
 
       <Link
         to={`/product/${product.id}`}
       >
+
         View Details
+
       </Link>
 
 
     </div>
 
   );
+
 }
 
 
