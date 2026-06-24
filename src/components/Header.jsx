078387e4a5d74e851848b5a18b0d9676ab2251bd
deepakgoyal106 <deepakgoyal106@gmail.com>
2026-06-24 16:11:44 +0530

    Add comments in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,25 @@
+// Header component
+// Displays application name and navigation links
+// Shows total number of items in cart
+
+
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 
 function Header() {
 
+
+  // Get cart items from Redux store
   const cartItems = useSelector(
-    (state)=>state.cart.cartItems
+    (state) => state.cart.cartItems
   );
 
 
+  // Calculate total quantity of products
   const totalItems = cartItems.reduce(
-    (total,item)=>total + item.quantity,
+    (total, item) =>
+      total + item.quantity,
     0
   );
 
@@ -19,21 +28,34 @@ function Header() {
 
     <header>
 
+
+      {/* Application title */}
+
       <h2>
         ShoppyGlobe
       </h2>
 
 
+
       <nav>
+
+
+        {/* Navigate to Home page */}
 
         <Link to="/">
           Home
         </Link>
 
 
+
+        {/* Navigate to Cart page and show count */}
+
         <Link to="/cart">
+
           Cart ({totalItems})
+
         </Link>
+
 
       </nav>
 
@@ -41,6 +63,7 @@ function Header() {
     </header>
 
   );
+
 }
 
 
