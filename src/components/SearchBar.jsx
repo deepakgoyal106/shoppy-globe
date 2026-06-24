c5a0eaa1f7e9bf159348088475e7a74ca679e92e
deepakgoyal106 <deepakgoyal106@gmail.com>
2026-06-24 16:24:31 +0530

    search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,29 +1,65 @@
+// SearchBar component
+// Takes user input and updates Redux search state
+// ProductList uses this value to filter products
+
+
 import { useDispatch } from "react-redux";
+
 import { setSearchTerm } from "../redux/slices/searchSlice";
+
 
 
 function SearchBar(){
 
+
+
+  // Access Redux dispatch
+
   const dispatch = useDispatch();
+
+
 
 
   return (
 
+
     <div className="search-box">
 
+
+
+      {/* 
+        Whenever user types,
+        update search text in Redux store
+      */}
+
       <input
+
         type="text"
+
         placeholder="Search products..."
-        onChange={(e)=>
+
+        onChange={(e)=>{
+
           dispatch(
-            setSearchTerm(e.target.value)
-          )
-        }
+
+            setSearchTerm(
+              e.target.value
+            )
+
+          );
+
+        }}
+
       />
+
+
 
     </div>
 
+
   );
+
+
 }
 
 
